20a306d937446a115d8c41d7fda409db00dc2c5a
Parixit Chaudhary <155245194+chparixit@users.noreply.github.com>
2026-06-18 12:14:13 +0545

    chnages in uplaod

--- backend/src/controller/authController.ts
+++ backend/src/controller/authController.ts
@@ -80,6 +80,7 @@ export const login = async (req: Request, res: Response) => {
         id: user._id,
         name: user.name,
         email: user.email,
+        profileImage: user.profileImage || "",
       },
     });
   } catch (error: any) {
