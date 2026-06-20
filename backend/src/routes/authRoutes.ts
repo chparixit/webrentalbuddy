0f8163e727d5b93e1157631332f04eae2ca9a53f
Parixit Chaudhary <155245194+chparixit@users.noreply.github.com>
2026-06-20 23:35:06 +0545

    authroutes

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,9 +1,16 @@
 import express from "express";
-import { register, login } from "../controller/authController";
+import { register, login, whoami, updateProfile, updatePassword } from "../controller/authController";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { upload } from "../middlewares/upload";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+
+// Protected routes
+router.get("/whoami", authMiddleware, whoami);
+router.put("/update", authMiddleware, upload.single("profileImage"), updateProfile);
+router.put("/update-password", authMiddleware, updatePassword);
 
 export default router;
